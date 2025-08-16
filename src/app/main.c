fe88f3456f5b774be6c4452c72bb7338dc23ff29
MickeyH@Yoga2 <hozmickey@gmail.com>
2025-08-17 01:24:54 +0300

    added code to simulate a a display buffer being manipulated across the platform api

--- src/app/main.c
+++ src/app/main.c
@@ -8,7 +8,7 @@ void app_init(Platform_t *platform, uint8_t *state_mem, size_t state_mem_len)
     printf("app init.\n");
 
     state_mem[0] = platform->gfx_load_texture("thing");
-    state_mem[1] = 123;
+    state_mem[1] = platform->gfx_load_texture("other thing");
 }
 
 void app_loop(Platform_t *platform, uint8_t *state_mem, size_t state_mem_len)
@@ -16,7 +16,7 @@ void app_loop(Platform_t *platform, uint8_t *state_mem, size_t state_mem_len)
     printf("app loop.\n");
 
     platform->input_read();
+    platform->gfx_clear_buffer();
     platform->gfx_draw_texture(state_mem[0], 12, 34);
     platform->gfx_draw_texture(state_mem[1], 4, 5);
-    platform->gfx_clear_buffer();
 }
--- src/platforms/test/main.c
+++ src/platforms/test/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <dlfcn.h>                                                                                                                                                                                                                        
 #include <time.h>                                                                                                                                                                                                                         
@@ -10,6 +11,9 @@
 #ifndef LIB_NAME
 #define LIB_NAME ""
 #endif
+
+#define MOCK_TEXTURE_MAX_COUNT (32)
+#define MOCK_SPRITE_MAX_COUNT (32)
 
 static const char *app_init_name = "app_init";
 static const char *app_loop_name = "app_loop";
@@ -25,27 +29,69 @@ static void (*func_handle)(void) = NULL;
 static void (*app_init)(Platform_t *platform, uint8_t *state_mem, size_t state_mem_len);
 static void (*app_loop)(Platform_t *platform, uint8_t *state_mem, size_t state_mem_len);
 
+typedef struct MockTexture
+{
+    char name[32];
+} MockTexture_t;
+
+typedef struct MockSprite
+{
+    int texture_idx;
+    int x;
+    int y;
+} MockSprite_t;
+
+static uint8_t mock_texture_count = 0;
+static uint8_t mock_sprite_count = 0;
+
+static MockTexture_t mock_texture_storage[MOCK_TEXTURE_MAX_COUNT] = {0};
+static MockSprite_t mock_sprite_buffer[MOCK_SPRITE_MAX_COUNT] = {0};
 
 void gfx_clear_buffer(void)
 {
+    mock_sprite_count = 0;
     printf("gfx clear buffer.\n");
 }
 
 int gfx_load_texture(char *name)
 {
+    if (mock_texture_count >= MOCK_TEXTURE_MAX_COUNT) return -1;
+
+    strncpy(mock_texture_storage[mock_texture_count].name, name, 32);
     printf("gfx load texture: %s.\n", name);
-    return 0;
+    mock_texture_count++;
+    return mock_texture_count-1;
 }
 
 void gfx_draw_texture(int idx, int x, int y)
 {
+    if (mock_sprite_count >=MOCK_SPRITE_MAX_COUNT) return;
+
     printf("gfx draw texture: idx %d, x %d, y %d.\n", idx, x, y);
+    mock_sprite_buffer[mock_sprite_count].texture_idx = idx;
+    mock_sprite_buffer[mock_sprite_count].x = x;
+    mock_sprite_buffer[mock_sprite_count].y = y;
+    mock_sprite_count++;
 }
 
 char input_read(void)
 {
     printf("input read.\n");
     return 0;
+}
+
+static void gfx_sync_buffer(void)
+{
+    printf("== MOCK BUFFER SYNC:\n");
+
+    for (int i = 0; i < mock_sprite_count; i++)
+    {
+        printf("  %s at [%d, %d]\n",
+                mock_texture_storage[mock_sprite_buffer[i].texture_idx].name,
+                mock_sprite_buffer[i].x, mock_sprite_buffer[i].y);
+    }
+
+    printf("   END MOCK BUFFER SYNC ==\n");
 }
 
 static void load_dynamic(void)
@@ -121,6 +167,8 @@ int main(int argc, char **argv)
         else
         {
         }
+
+        gfx_sync_buffer();
 
         stat(lib_path, &file_stat);
         lib_modified_time = file_stat.st_mtime;
